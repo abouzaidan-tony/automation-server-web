2789280c702e43305ba13db5818ef39abc1f8be3
Tony <abouzaidan.tony@gmail.com>
2020-03-20 23:43:56 +0300

    Remove unity invoice input

--- src/main/java/com/tony/automationserverweb/form/DevAccountForm.java
+++ src/main/java/com/tony/automationserverweb/form/DevAccountForm.java
@@ -1,21 +1,11 @@
 package com.tony.automationserverweb.form;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.tony.automationserverweb.model.DevAccount;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 
 @JsonIgnoreProperties({ "validated", "errors" })
 public class DevAccountForm implements Form<DevAccount> {
@@ -75,68 +65,68 @@ public class DevAccountForm implements Form<DevAccount> {
         if (userInvoice == null)
             userInvoice = "";
 
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
-        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-        headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
-        String fooResourceUrl = "https://api.assetstore.unity3d.com/publisher/v1/invoice/verify.json?key=AOYGTWPa1ICM7Xvl6VEV0K0cYOg&invoice="+userInvoice;
-        ResponseEntity<String> response = null;
-        try{
-            response = restTemplate.exchange(fooResourceUrl, HttpMethod.GET, entity, String.class);
-        }catch(Exception ex){
-            ex.printStackTrace();
-        }
+        // RestTemplate restTemplate = new RestTemplate();
+        // HttpHeaders headers = new HttpHeaders();
+        // headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        // headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+        // HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+        // headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+        // String fooResourceUrl = "https://api.assetstore.unity3d.com/publisher/v1/invoice/verify.json?key=AOYGTWPa1ICM7Xvl6VEV0K0cYOg&invoice="+userInvoice;
+        // ResponseEntity<String> response = null;
+        // try{
+        //     response = restTemplate.exchange(fooResourceUrl, HttpMethod.GET, entity, String.class);
+        // }catch(Exception ex){
+        //     ex.printStackTrace();
+        // }
         
         
-        boolean isWrong = true;
+        // boolean isWrong = true;
 
-        do{
+        // do{
 
-            if(userInvoice.length() == 0) {
-                isWrong = false;
-                break;
-            }
+        //     if(userInvoice.length() == 0) {
+        //         isWrong = false;
+        //         break;
+        //     }
 
-            if(response == null)
-                break;
+        //     if(response == null)
+        //         break;
 
             
 
-            if(!response.getStatusCode().equals(HttpStatus.OK))
-                break;
+        //     if(!response.getStatusCode().equals(HttpStatus.OK))
+        //         break;
 
-            JSONObject obj = new JSONObject(response.getBody());
-            if(!obj.has("invoices"))
-                break;
-            isWrong = false;
+        //     JSONObject obj = new JSONObject(response.getBody());
+        //     if(!obj.has("invoices"))
+        //         break;
+        //     isWrong = false;
 
-            Object invoicesObj = obj.get("invoices");
-            JSONArray invoices = null;
-            if(invoicesObj instanceof JSONArray)
-                invoices = (JSONArray) invoicesObj;
-            else {
-                errors.put("unityInvoice", "Invalid unity invoice");
-                break;
-            }
+        //     Object invoicesObj = obj.get("invoices");
+        //     JSONArray invoices = null;
+        //     if(invoicesObj instanceof JSONArray)
+        //         invoices = (JSONArray) invoicesObj;
+        //     else {
+        //         errors.put("unityInvoice", "Invalid unity invoice");
+        //         break;
+        //     }
 
-            if(invoices.length() == 0)
-                errors.put("unityInvoice", "Invalid unity invoice");
-            else if(invoices.length() != 1)
-                errors.put("unityInvoice", "Please submit 1 invoice number");
-            else {
-                JSONObject invoice = invoices.getJSONObject(0);
-                String invoicePackage = invoice.getString("package");
-                if(!"ESP WiFi plugin".equals(invoicePackage)){
-                    errors.put("unityInvoice", "This invoice is for " + invoicePackage + " and not for ESP WiFi plugin");
-                }
-            }
+        //     if(invoices.length() == 0)
+        //         errors.put("unityInvoice", "Invalid unity invoice");
+        //     else if(invoices.length() != 1)
+        //         errors.put("unityInvoice", "Please submit 1 invoice number");
+        //     else {
+        //         JSONObject invoice = invoices.getJSONObject(0);
+        //         String invoicePackage = invoice.getString("package");
+        //         if(!"ESP WiFi plugin".equals(invoicePackage)){
+        //             errors.put("unityInvoice", "This invoice is for " + invoicePackage + " and not for ESP WiFi plugin");
+        //         }
+        //     }
 
-        }while(false);
+        // }while(false);
        
-        if(isWrong)
-            errors.put("unityInvoice", "Error occured");
+        // if(isWrong)
+        //     errors.put("unityInvoice", "Error occured");
     }
 
     public DevAccount fill(){
--- src/main/webapp/WEB-INF/jsp/dev_signup.jsp
+++ src/main/webapp/WEB-INF/jsp/dev_signup.jsp
@@ -54,13 +54,13 @@
                         </span>
                     </div>
 
-                    <div class="wrap-input100 validate-input" data-validate="Enter valid invoice number">
+                    <!-- <div class="wrap-input100 validate-input" data-validate="Enter valid invoice number">
                         <form:input path="userInvoice" class="input100" type="text" name="userInvoice" placeholder="Unity Invoice number" />
                         <span class="focus-input100"></span>
                         <span class="symbol-input100">
                             <i class="fa fa-file" aria-hidden="true"></i>
                         </span>
-                    </div>
+                    </div> -->
 
                     <div class="wrap-input100 validate-input" data-validate="Password is required">
                         <form:input path="userPassword" class="input100" type="password" name="userPassword"
